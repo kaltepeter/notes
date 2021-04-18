b736353ca83e015922c7c93bb5bae16a622e012d
Kayla Altepeter <kayla@kaylaaltepeter.com>
2021-04-17 22:33:34 -0500

    chore: add date fields

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const result = await graphql(`
     query {
       allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
+        sort: { order: DESC, fields: [frontmatter___date, fields___slug] }
       ) {
         edges {
           node {
@@ -27,7 +27,6 @@ exports.createPages = async ({ graphql, actions }) => {
               slug
             }
             frontmatter {
-              date(formatString: "MMMM DD, YYYY")
               title
               tags
             }
