2d2c44bc6e9d17c5b5207ede641bbed174f0c0b7
kaltepeter <5103752+kaltepeter@users.noreply.github.com>
2022-04-03 04:01:30 +0000

    Updates

--- component---src-pages-404-tsx-6e4fad4bc5345165054d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),o=n("9Dj+"),l=n("H8eV");t.default=function(){return u.a.createElement(o.a,null,u.a.createElement(l.a,{title:"404: Not found"}),u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-6e4fad4bc5345165054d.js.map
