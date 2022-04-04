d830d28af02002f4a19b796f9ab5cdd33f5a38f4
kaltepeter <5103752+kaltepeter@users.noreply.github.com>
2022-04-04 03:15:13 +0000

    Updates

---
+++ component---src-pages-404-tsx-c6735bfed6aa863ec331.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[218],{7735:function(t,e,n){n.r(e);var u=n(7294),a=n(1478),l=n(6969);e.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-c6735bfed6aa863ec331.js.map
