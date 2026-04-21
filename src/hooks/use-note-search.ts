2d5c6a5a810c6ddfe8960c4d4643f20a6365b3a8
kaltepeter <kayla@kaylaaltepeter.com>
2026-04-21 15:35:28 -0700

    fix: package engine/audit, bugs

--- src/hooks/use-note-search.ts
+++ src/hooks/use-note-search.ts
@@ -3,34 +3,31 @@ import * as JsSearch from "js-search";
 import { useNoteExcerptList } from "./use-note-excerpt-list";
 import { Note } from "../models/note";
 
+export const MIN_QUERY_LENGTH = 3;
+
 export const useNoteSearch = (query: string): Note[] => {
   const { notes } = useNoteExcerptList();
+  const isSearchEnabled = query.trim() !== "";
 
-  const documents = useMemo(
-    () =>
-      notes
-        .map(({ node }) => node as unknown as Note)
-        .filter(
-          (note) =>
-            typeof note.frontmatter?.title === "string" &&
-            note.frontmatter.title.trim() !== "" &&
-            typeof note.fields?.slug === "string" &&
-            note.fields.slug.trim() !== "",
-        ),
-    [notes],
-  );
+  return useMemo(() => {
+    if (!isSearchEnabled) return [];
 
-  const index = useMemo(() => {
+    const documents = notes
+      .map(({ node }) => node as unknown as Note)
+      .filter(
+        (note) =>
+          typeof note.frontmatter?.title === "string" &&
+          note.frontmatter.title.trim() !== "" &&
+          typeof note.fields?.slug === "string" &&
+          note.fields.slug.trim() !== "",
+      );
+
     const search = new JsSearch.Search("id");
     search.addIndex(["frontmatter", "title"]);
     search.addIndex(["frontmatter", "tags"]);
     search.addIndex("excerpt");
     search.addDocuments(documents);
-    return search;
-  }, [documents]);
 
-  return useMemo(() => {
-    if (!query.trim()) return [];
-    return index.search(query) as Note[];
-  }, [query, index]);
+    return search.search(query) as Note[];
+  }, [isSearchEnabled, notes, query]);
 };
